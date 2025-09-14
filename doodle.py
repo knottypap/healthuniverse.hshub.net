dfa1710d465d0e513322e52a9195c9f73010ded9
Deborah Wilson <bizdoozysneakers@gmail.com>
2025-09-14 20:58:20 +0100

    Update doodle.py
    Signed-off-by: Deborah Wilson <bizdoozysneakers@gmail.com>

--- doodle.py
+++ doodle.py
@@ -1,3 +1,3 @@
 https://replit.com/@4y6ttr7xpb/
 ForexSignalVue?
-mobileWebview=1&forceTheme=replitDark&Bonsai=2,152.13&errorTrace=0ge7cc80-191a-41fa-ad4a-4731b4d9ae34&mobileBridge=1&hideBottomBar=1&supportsUpsellBridge=1&permissionRequest=1&speechRecogntion=1&workspace2=1&webviewBridge=1&storeReviewPrompt=1&initialPaneType=deployments#app.py
+mobileWebview=1&forceTheme=replitDark&vBonsai=2.152.13&errorTrace=0ge7cc80-191a-41fa-ad4a-4731b4d9ae34&mobileBridge=1&hideBottomBar=1&supportsUpsellBridge=1&permissionRequest=1&speechRecogntion=1&workspace2=1&webviewBridge=1&storeReviewPrompt=1&initialPaneType=deployments#app.py
